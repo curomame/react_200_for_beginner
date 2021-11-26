05ed22b6c44a7028367dbba8c0678693661fd58a
HyangRim <curomame@naver.com>
2021-11-27 01:18:27 +0900

    to 006

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import LifecycleEx from './components/R007_LifecycleEx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Start React 200!</h1>  
+    <p>CSS 적용하기</p>
+    <LifecycleEx
+    prop_value = 'FromApp.js'
+    />
+  </div>
   );
 }
 
